a199a9aabd8861b368b2b3a1df0c098691d38dbf
nonocro <87670992+noaBr@users.noreply.github.com>
2025-11-06 10:53:51 +0100

    FEAT: Login page

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 // Constantes pour SharedPreferences
 const String kOnboardingCompletedKey = 'onboarding_completed';
 
@@ -9,5 +7,6 @@ class RouteNames {
   static const String home = '/home';
   static const String onboarding = '/onboarding';
   static const String auth = '/auth';
+  static const String profile = '/profile';
 }
 
