9f439c5c7971d29d4b6de19de99b790c6f3095bb
MathisMchd <michenaudmathis@gmail.com>
2025-11-06 08:19:17 +0100

    ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.grape"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.3.13750724"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
